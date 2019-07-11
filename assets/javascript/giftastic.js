363432cbfd90bd94611c8144e29ec406c48c0956
Ying Huang <huangying270@gmail.com>
2019-07-11 19:37:22 -0400

    pseudo coded JS

---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+})
+
+/*
+variables for gifs of interest
+method GET to get data from giphy api
+generate gifs from api
+changing state of images, still and animate
+on click function for button to generate gifs
+*/
