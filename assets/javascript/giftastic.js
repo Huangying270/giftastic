bea3013ecfdb0de192b10ad22fac1cfb2b09b69d
Ying Huang <huangying270@gmail.com>
2019-07-11 22:53:17 -0400

    added on click function for button GET information for api, displays rating

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -8,22 +8,51 @@ $(document).ready(function(){
         $("#display-gif").empty();
 
         var input = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input +"&limit=10&api_key=qxs3QpdBNOIc9DigO3PmRnwXyZvadJYv";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input +"&limit=10&api_key=Kcj2pSMDpIaD2MCZxbsaZc4vX4NtAh0B";
 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
 
-            
+            console.log(response);
 
-        })
+            var results = response.data;
+
+            for(var i = 0; i < 10; i++) {
+
+                var newDiv = $("<div>");
+                newDiv.addClass("gifholder");
+
+                $("<img>").attr("src", results[i].images.original_still.url);
+                $("<img>").attr("data-still", results[i].images.original_still.url);
+                $("<img>").attr("data-animate", results[i].images.original.url);
+                $("<img>").attr("data-state", "still");
+                $("<img>").attr("class", "gif");
+                newDiv.append($("<img>"));
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+                newDiv.append(p)
+
+                $("#display-gif").append(newDiv);
+
+            }
+
+        });
 
     }
 
+    $("#confirmButton").on("click", function(){
 
+        var input = $("#user-input").val();
+        displayedGiphy.push(input);
 
+        return;
+    })
 
+    $(document).on("click", displayGifs);
 
 
 
