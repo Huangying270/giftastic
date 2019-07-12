edc466a440d0a0819311f1b15efdc1437782a7ea
Ying Huang <huangying270@gmail.com>
2019-07-11 20:21:43 -0400

    giphy api and ajax

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
 
+    var displayedGiphy = ["Pikachu", "Eevee", "Brock", "Togepi"];
 
+    function displayGifs(){
+
+        // Clears displayed gifs
+        $("#display-gif").empty();
+
+        var input = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input +"&limit=10&api_key=qxs3QpdBNOIc9DigO3PmRnwXyZvadJYv";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+
+            
+
+        })
+
+    }
 
 
 
